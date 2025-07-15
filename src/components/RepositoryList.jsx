0f2004a2f9a69cb060bfa0ecc5b68ffe65783837
muniz-luiz <eusouluizeduardomuniz@gmail.com>
2025-07-15 08:09:28 -0300

    feat: integração com API do Github e renderização dinâmica de respositórios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,16 +1,23 @@
 import { RepositoryItem } from "./RepositoryItem";
-
-const repositoryName = "unform2";
+import { useState, useEffect } from "react";
+import "../styles/repositories.scss";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
-    <section className="repository-List">
+    <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem repository="DC Comics" />
-        <RepositoryItem />
-        <RepositoryItem />
-        <RepositoryItem />
+        {repositories.map((repository) => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   );
